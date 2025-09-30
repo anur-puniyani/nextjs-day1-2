da472d28461d6c68718d594ba599b6e3fd53d177
anur-puniyani <anur_p@yahoo.co.uk>
2025-09-30 14:59:58 +0530

    changed anchor to link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 export default function RootLayout({
   children,
 }: {
@@ -7,8 +8,13 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <nav>
-          <a href="/">Home</a> | <a href="/about">About</a> |{" "}
-          <a href="/contact">Contact</a>
+          <Link href="/" className="mr-4">
+            Home
+          </Link>
+          <Link href="/about" className="mr-4">
+            About
+          </Link>
+          <Link href="/contact">Contact</Link>
         </nav>
         {children}
       </body>
